7ec1c2f9470775ce679d0e60f9b3a96eafd35b44
benevolent-trace-0172 <davidmlee4@gmail.com>
2020-01-21 04:22:26 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -35,8 +35,8 @@ def monopoly_with_fourth_tier
       four_pieces_owned: 200},
   :names => {
     :reading_railroad => {"mortgage_value" => "$100"},
-    :pennsylvania_railroad => {mortgage_value: $1000},
-    :b_and_o_railroad => {mortgage_value: $1000},
-    :shortline_railroad => {mortgage_value: $1000}
+    :pennsylvania_railroad => {"mortgage_value" => "200"},
+    :b_and_o_railroad => {"mortgage_value" => $300},
+    :shortline_railroad => {"mortgage_value" => $1000}
     }}}
 end
